46693d9964f5221a17a34f05f8502d01762ac9ed
Hsiang Chou <hsiang@tibbo.com>
2021-11-30 14:15:45 +0800

    Testing chunked response

--- http-server/server.js
+++ http-server/server.js
@@ -3,7 +3,23 @@ var url = require('url');
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    var q = url.parse(req.url, true).query;
-    var txt = q.data;
-    res.end(txt);
+    var path = url.parse(req.url, true).pathname;
+    switch (path) {
+        case '/chunked':
+            res.write("chunk1");
+            res.write("chunk2");
+            res.write("chunk3");
+            res.write("chunk4");
+            res.write("chunk5");
+            res.end();
+            break;
+        case '/notChunked':
+            var q = url.parse(req.url, true).query;
+            var data = q.data;
+            res.end(data);
+            break;
+        case '/noContent':
+            res.end();
+            break;
+    }
 }).listen(8800);
